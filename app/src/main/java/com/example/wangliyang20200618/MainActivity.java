1a5682f686529296dbc8262b1bb940bc28cd301b
364086070@qq.com <wly970814>
2020-06-18 14:54:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wangliyang20200618/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.wangliyang20200618;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /*
+    * 第一次
+    * */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
