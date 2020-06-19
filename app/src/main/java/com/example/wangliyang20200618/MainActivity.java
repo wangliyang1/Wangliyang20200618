649a932799538e827a64836d5ae7f29c2663f1a6
72087327 <137478494@qq.com>
2020-06-19 09:47:44 +0800

    第一次修改的东西

--- app/src/main/java/com/example/wangliyang20200618/MainActivity.java
+++ app/src/main/java/com/example/wangliyang20200618/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //你看看代码修改了没
 
 
     }
