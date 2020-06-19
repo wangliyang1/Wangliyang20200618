96ca803c711944ab706c8e1ed4d5affff224c113
wangliyang <wangliyang0814@163.com>
2020-06-19 09:50:35 +0800

    three

--- app/src/main/java/com/example/wangliyang20200618/MainActivity.java
+++ app/src/main/java/com/example/wangliyang20200618/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //你看看代码修改了没
-
+        //改完了
 
     }
 }
